95303b7c6b68eb6f322baeb9f5cb062b969760e0
Yasmin <yasminnadhifa.bts@gmail.com>
2025-10-07 10:50:33 +0900

    feat: add logo

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,22 @@ function App() {
     <div className="min-h-screen bg-gradient-to-b from-purple-100 via-white to-purple-50 text-gray-800 font-sans">
       {/* Header */}
       <header className="p-6 bg-gradient-to-r from-purple-700 via-purple-500 to-purple-400 text-white shadow-lg">
-        <h1 className="text-3xl font-extrabold tracking-wide">
-          📈 Dashboard Aspirasi Masyarakat
-        </h1>
+        <div className="flex items-center space-x-4">
+          <img src="/logo.png" alt="Logo" className="h-12 w-15" />
+          <h1 className="text-3xl font-extrabold tracking-wide">
+            SIMO’ODI (Sistem Informasi Monitoring Data Aspirasi)
+          </h1>
+        </div>
       </header>
 
       {/* Main */}
       <main className="p-6">
         <DashboardAspirasi />
       </main>
-     <FloatingHelpButton></FloatingHelpButton>
+      <FloatingHelpButton></FloatingHelpButton>
       {/* Footer */}
       <footer className="p-4 text-center bg-gradient-to-r from-yellow-400 via-orange-300 to-purple-400 text-gray-900 font-medium shadow-inner mt-6">
-        © {new Date().getFullYear()} 
+        © {new Date().getFullYear()}
       </footer>
     </div>
   );
